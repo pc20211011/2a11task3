6b04b0fc2961798e21ce2c54b39d3948b2e589b3
pc20211011 <115674716+pc20211011@users.noreply.github.com>
2022-10-20 09:36:56 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+input.onButtonPressed(Button.A, function () {
+    while (count >= 0) {
+        basic.showNumber(count)
+        count += -1
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    count = -1
+})
+let count = 0
+count = 9
